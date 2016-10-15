6f9ce6936d9c90de935223097699b6293667b692
Connor Lundberg <connorlndbrg349@gmail.com>
2016-10-14 20:46:39 -0700

    rough bullet pattern
    I have a working bullet pattern that will shoot the specified shot count
    in a circle around the object at the specified speed. It can only go in
    a circle.

---
+++ gdcFall16/Assets/Scripts/BulletPattern.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This class is also temporary. It is just here for organization.
+[System.Serializable]
+public class BulletDetails {
+	public float fireRate, delay, bulletSpeed;
+	public int shotCount;
+}
+
+public class BulletPattern : MonoBehaviour {
+
+	//These will be changed to private as this bullet pattern
+	//will be instantiated by a later bullet system that
+	//will be applied to all enemies.
+
+	public Rigidbody shot;
+	public Transform shotSpawn;
+	public BulletDetails bulletDetails;
+
+	//This isn't in BulletDetails because it can't be calculated by 
+	//an uninitialized shotCount field so it has be done here.
+	private float shotAngle;
+
+	void Start ()
+	{
+		//shotAngle is used to space the bullets out evenly. Will
+		//be updated if we want non-circular firing abilities.
+		shotAngle = 360 / bulletDetails.shotCount;
+		InvokeRepeating ("Fire", bulletDetails.delay, bulletDetails.fireRate);
+	}
+
+	void Fire ()
+	{
+		//This will instantiate the specified number of bullets at the specified directions
+		for (int i = 0; i < bulletDetails.shotCount; i++) {
+			GameObject clone = Instantiate (shot, shotSpawn.position, 
+				Quaternion.Euler(new Vector3 (0.0f, shotAngle * (i + 1), 0.0f))) as GameObject;
+			Rigidbody temp = clone.GetComponent<Rigidbody> ();
+			temp.velocity = transform.TransformDirection(transform.forward * bulletDetails.bulletSpeed);
+		}
+	}
+}
--- gdcFall16/Assets/Scripts/Done_Mover.cs
+++ gdcFall16/Assets/Scripts/Done_Mover.cs
@@ -5,8 +5,11 @@ public class Done_Mover : MonoBehaviour
 {
 	public float speed;
 
+	//This is the default speed for the bullets. Bullet patterns for enemies 
+	//will override this.
 	void Start ()
 	{
 		GetComponent<Rigidbody>().velocity = transform.forward * speed;
+		//Debug.Log ("The velocity is: " + GetComponent<Rigidbody> ().velocity);
 	}
 }
