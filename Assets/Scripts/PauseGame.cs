fb35d66020967e5c49576809fe96384ea35307a6
Ba2012 <ba2012@uw.edu>
2016-11-10 10:35:46 -0800

    Pause Menu
    Resume works on click
    Escape toggles pause/unpause

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame : MonoBehaviour {
+    public Transform Canvas;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+    public void Pause()
+    {
+        if (Canvas.gameObject.activeInHierarchy == false)
+        {
+
+            Canvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Canvas.gameObject.SetActive(false);
+            Time.timeScale = 1;
+
+        }
+    }
+
+
+}
+
