b39526785f7f349869c434341aa6855e432de15e
Connor Lundberg <connorlndbrg349@gmail.com>
2016-11-15 17:13:45 -0800

    Made music changes

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class Music : MonoBehaviour {
     public AudioClip[] backgroundMusic; 
+	private AudioSource source;
+	int i =0;
 	// Use this for initialization
 	void Start () {
-	
+		source = GetComponent<AudioSource> ();
+		source.clip = backgroundMusic[i];
+
+		source.Play ();
+
+
 	}
-	
 	// Update is called once per frame
 	void Update () {
+		if (!source.isPlaying) {
+			i = (i+1)%backgroundMusic.Length;
+			source.clip = backgroundMusic[i];
+			source.Play ();
+
+		}
 	
 	}
 }
