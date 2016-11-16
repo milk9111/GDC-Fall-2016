4bccbdb05ea70bce15d6460199afa53b4a686900
Ba2012 <ba2012@uw.edu>
2016-11-16 12:56:07 -0800

    Pause menu and resume works

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -3,28 +3,38 @@ using System.Collections;
 
 public class PauseGame : MonoBehaviour {
     public Transform Canvas;
+    public bool restart;
 
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            restart = false;
             Pause();
+            Restart();
         }
+
     }
     public void Pause()
     {
         if (Canvas.gameObject.activeInHierarchy == false)
         {
-
+            Cursor.visible = true;
             Canvas.gameObject.SetActive(true);
             Time.timeScale = 0;
         }
         else
         {
+            Cursor.visible = false;
             Canvas.gameObject.SetActive(false);
             Time.timeScale = 1;
 
         }
+    }
+
+    public void Restart()
+    {
+       
     }
 
 
