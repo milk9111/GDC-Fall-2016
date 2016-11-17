875971d2370dc9371f75dcfce0dce5e5f7b40a46
Ba2012 <ba2012@uw.edu>
2016-11-17 14:55:47 -0800

    No cursor on start

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -5,6 +5,12 @@ public class PauseGame : MonoBehaviour {
     public Transform Canvas;
     public bool restart;
 
+
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.Escape))
