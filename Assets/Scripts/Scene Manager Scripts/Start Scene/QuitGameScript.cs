c1b2328273b1dd90ebf441ef37ca7b126119f9d9
Bishop <johnsbis@uw.edu>
2016-12-03 13:09:29 -0800

    Start Menu Update
    -Added a Start Menu with some functionality.
    -Adjusted some game UI elements.
    -Added subfolder in scripts "Scene Manager Scripts".

---
+++ Assets/Scripts/Scene Manager Scripts/Start Scene/QuitGameScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class QuitGameScript : MonoBehaviour {
+
+	void Start() {
+
+		Button thisButton = this.transform.GetComponent<Button>();
+		thisButton.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick() {
+
+		Debug.Log("Quit was pressed");
+
+		//Application.Quit();
+	}
+}
---
+++ Assets/Scripts/Scene Manager Scripts/Start Scene/StartGameScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StartGameScript : MonoBehaviour {
+
+	void Start() {
+
+		Button thisButton = this.transform.GetComponent<Button>();
+		thisButton.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick() {
+
+		Debug.Log("Start was pressed");
+
+		SceneManager.LoadSceneAsync("Scenes/Done_Main", LoadSceneMode.Single);
+	}
+}
